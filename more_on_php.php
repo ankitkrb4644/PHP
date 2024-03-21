d8b11f5e46b72e43b398b4af8dbdb8e1834e3c42
Ankit <ankitkrb4644@gmail.com>
2024-03-21 23:16:43 +0530

    update

--- more_on_php.php
+++ more_on_php.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP</title>
 </head>
 <body>
-    
+<div class="container">
+        This is my first php website
+        <?php
+    define('PI',3.14);
+    echo"hello world and this is printed using php";
+    // variables
+    $variable1 = 37;
+    $variable2 = 47;
+    echo $variable1;
+    echo $variable2;
+    echo "<br>";
 </body>
 </html>
